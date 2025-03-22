e3cb4a24d31b68e5b8e71b762f53646fa7fbcd41
Mark-van-B <mark.vanboekholdt@student.ucll.be>
2025-03-22 14:05:45 +0100

    Aanpassing RWD menu

--- Website/script.js
+++ Website/script.js
@@ -1,6 +1,6 @@
 function actiefTab()
 {
-    const links = document.querySelectorAll('#Nav a');
+    const links = document.querySelectorAll('#List a');
     links.forEach(link => {
     if (link.href === window.location.href) {
         link.classList.add('active');
